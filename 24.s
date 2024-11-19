e624afb4246a0deb276984b3f106e3c2b1925576
Bryan Alejandro Rodriguez Gomez <158237554+BryanAlRG@users.noreply.github.com>
2024-11-19 10:10:32 -0800

    Update 24.s

--- 24.s
+++ 24.s
@@ -1,3 +1,21 @@
+# Instituto Tecnológico de Tijuana
+# Departamento de Sistemas y computación
+# Materia: Lenguajes de interfaz
+# Nombre del alumno: Rodríguez Gomez Bryan Alejandro 
+# No. control 22210345
+# Nombre del programa: Calcular la longitud de una cadena
+#Python
+def strlen(s: str) -> int:
+    length = 0
+    for char in s:
+        length += 1
+    return length
+
+# Ejemplo de uso:
+cadena = "Hello"
+longitud = strlen(cadena)
+print("La longitud de la cadena es:", longitud)
+#Arm
 .global strlen
 .type strlen, %function
 
@@ -12,3 +30,6 @@ count_loop:
     sub x0, x1, x0         // Calcular diferencia
     sub x0, x0, #1         // Ajustar por null terminator
     ret
+
+#Asciinema 
+https://asciinema.org/a/hubbiRXuOXaycjBIOjnJxkXjc
