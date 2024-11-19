85b0a1e42c0dcaf3e880c0f27955733e0a9f3692
Bryan Alejandro Rodriguez Gomez <158237554+BryanAlRG@users.noreply.github.com>
2024-11-19 00:59:05 -0800

    Update 12.s

--- 12.s
+++ 12.s
@@ -1,3 +1,33 @@
+# Instituto Tecnológico de Tijuana
+# Departamento de Sistemas y computación
+# Materia: Lenguajes de interfaz
+# Nombre del alumno: Rodríguez Gomez Bryan Alejandro 
+# No. control 22210345
+# Nombre del programa: 	Encontrar el máximo en un arreglo
+------------------------------------------------------------------------------------------------
+#Python 
+def find_max(arr):
+    """
+    Encuentra el valor máximo en un arreglo.
+
+    :param arr: Lista de números enteros o flotantes.
+    :return: El valor máximo en la lista.
+    """
+    if not arr:  # Si el arreglo está vacío, devolver None
+        return None
+
+    max_val = arr[0]  # Primer elemento como máximo inicial
+
+    for elem in arr[1:]:  # Iterar sobre los elementos restantes
+        if elem > max_val:  # Comparar con el máximo actual
+            max_val = elem  # Actualizar máximo si es mayor
+
+    return max_val
+
+# Ejemplo de uso
+arr = [3, 5, 1, 8, 2, 10, 6]
+print("El valor máximo es:", find_max(arr))
+-----------------------------------------------------------------------------------
 .global find_max
 .type find_max, %function
 
@@ -21,3 +51,6 @@ max_loop:
 max_done:
     mov x0, x2          // Retornar máximo
     ret
+
+    #    Asciinema 
+    https://asciinema.org/a/UgZVR4AV2GvbtbqfmKai8CN7B
