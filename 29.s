738e601e848b670a758aeda9248f8d1cad1d7979
Bryan Alejandro Rodriguez Gomez <158237554+BryanAlRG@users.noreply.github.com>
2024-11-19 10:32:49 -0800

    Update 29.s

--- 29.s
+++ 29.s
@@ -1,3 +1,25 @@
+# Instituto Tecnológico de Tijuana
+# Departamento de Sistemas y computación
+# Materia: Lenguajes de interfaz
+# Nombre del alumno: Rodríguez Gomez Bryan Alejandro 
+# No. control 22210345
+# Nombre del programa: Contar los bits activados en un número
+
+#Python
+def count_set_bits(number):
+    count = 0
+    while number > 0:
+        count += number & 1  # Incrementar el contador si el último bit es 1
+        number >>= 1          # Desplazar a la derecha
+    return count
+
+# Ejemplo de uso
+number = 0b10101010  # Número binario (170 en decimal)
+
+set_bits = count_set_bits(number)
+print(f"Número de bits establecidos en {bin(number)}: {set_bits}")
+
+#Ensamblador 
 .global count_set_bits
 .type count_set_bits, %function
 
@@ -18,3 +40,5 @@ count_loop:
 done:
     mov x0, x1             // Retornar cantidad de bits
     ret
+#Asciinema 
+https://asciinema.org/a/ULap2NlRwweex2UZ6XjiJXRcs
